6c79ec6610295633f2286599692505ed3b96f84c
beingvisky <vishak.28@gmail.com>
2020-12-18 16:31:55 +0530

    calc update

--- Demo/src/com/lti/model/Calc.java
+++ Demo/src/com/lti/model/Calc.java
@@ -2,11 +2,11 @@ package com.lti.model;
 
 public class Calc {
 	public void add(int num1,int num2) {
-		System.out.println("Sun=" +(num1+num2));
+		System.out.println("Summation=" +(num1+num2));
 	}
 	
 	public void sub(int num1,int num2) {
-		System.out.println("Sun=" +(num1+num2));
+		System.out.println("Sun=" +num1+num2);
 	}
 	
 	
