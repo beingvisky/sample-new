4a736b44c0dc6769031509c78acb466983dbd03c
beingvisky <vishak.28@gmail.com>
2020-12-18 16:57:53 +0530

    master commit

--- Demo/src/com/lti/model/Calc.java
+++ Demo/src/com/lti/model/Calc.java
@@ -5,6 +5,11 @@ public class Calc {
 		System.out.println("Summation=" +(num1+num2));
 	}
 	
+	public void sub(int num1,int num2) {
+		System.out.println("Summation=" +(num1+num2));
+	}
+	
+	
 
 	
 	
