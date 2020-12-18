f4850db6313c007d24be9c8a6830e920c0bbd214
beingvisky <vishak.28@gmail.com>
2020-12-18 16:11:15 +0530

    first

---
+++ Demo/src/com/lti/model/Calc.java
@@ -0,0 +1,13 @@
+package com.lti.model;
+
+public class Calc {
+	public void add(int num1,int num2) {
+		System.out.println("Sun=" +(num1+num2));
+	}
+	
+	public void sub(int num1,int num2) {
+		System.out.println("Sun=" +(num1+num2));
+	}
+	
+	
+}
---
+++ Demo/src/com/lti/model/Sample.java
@@ -0,0 +1,13 @@
+package com.lti.model;
+
+public class Sample {
+	public void show() {
+		System.out.println("Hello World");
+	}
+	
+	public void hello() {
+		System.out.println("Hello World");
+	}
+}
+
+
